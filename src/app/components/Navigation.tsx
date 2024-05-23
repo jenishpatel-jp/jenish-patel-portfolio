73faec25b87e16d8871639945e78ea79792a0139
Jenish <jpat534@gmail.com>
2024-05-23 16:02:10 +1000

    Added Navigation header

---
+++ src/app/components/Navigation.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import React from 'react'
+
+const Navigation = () => {
+    return (
+        <div className='flex flex-row p-3 justify-end bg-stone-800 text-orange-400' >
+            <h2 className='nav'>Home</h2>
+            <h2 className='nav'>About</h2>
+            <h2 className='nav'>Projects</h2>
+            <h2 className='nav'>Contact</h2>
+        </div>
+    )
+}
+
+export default Navigation
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-2">
-      <h1>Jenish Patel Portfolio</h1>
-      <p>Under construction</p>
+    <main className="flex min-h-screen flex-col bg-stone-900">
+      <div className="flex flex-col flex-1 place-content-center" >
+        <h1 className="text-white p-2 m-4" >Jenish Patel Portfolio</h1>
+        <p className="text-white p-2 m-4" >Under construction</p>
+      </div>
     </main>
   );
 }
