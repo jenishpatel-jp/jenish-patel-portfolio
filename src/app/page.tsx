84223a2380d6d59fadab696b3033950ecfd65093
Jenish <jpat534@gmail.com>
2024-05-23 17:03:19 +1000

    Updated the h2 tags so the quotation mark no longer causes error when deployed to vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center justify-center bg-stone-900">
         <h2 className="text-white w-full text-center text-5xl mb-5 " >
-          Hello, I'm <span className="text-orange-400" >Jenish</span>.
+          Hello, I&apos;m <span className="text-orange-400" >Jenish</span>.
         </h2>
         <h2 className="text-white w-full text-center text-5xl">
-          I'm a Front-End Engineer
+          I&apos;m a Front-End Engineer
         </h2>
     </main>
   );
