99038b9d2b5eac796ee89288bb5d7c2f83bb9f26
Jenish <jpat534@gmail.com>
2024-05-24 10:59:36 +1000

    Added about me paragraphy as wellas grid to hold the skills I have learnt

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,11 +6,29 @@ const Page = () => {
             <h1 className='my-4 text-5xl border p-3 w-full text-center' >About</h1>
             <div className='flex border w-5/6 mx-3 mt-4 mb-7 flex-1'>
                 <div className='border flex flex-col items-center flex-1' >
-                    <p>Image</p>
-                    <p>I&apos;m a Front End Engineer with a passion for continous improvement.</p>
+                    <div className='w-30 h-30 p-3 my-4 border'>
+                        <p>Image</p>
+                    </div>
+                    <p className='text-left'>
+                        I&apos;m a Front End Engineer with a passion for creating apps that make life easier. 
+                        <br/>
+                        I enjoy creating apps that solve issues using React and React Native.
+                        <br/>
+                        On a quest to continously learn and improve my skills so I can write better code.
+                        <br/>
+                        In my spare time I enjoy travelling, going to different restaurants/bars and gaming. 
+                    </p>
                 </div>
-                <div className='border flex flex-1'>
-
+                <div className='border grid grid-cols-3 grid-rows-3 gap-4 flex-1 p-2'>
+                    <p className='skills'>HTML</p>
+                    <p className='skills'>CSS</p>
+                    <p className='skills'>Javascript</p>
+                    <p className='skills'>React</p>
+                    <p className='skills'>Next.JS</p>
+                    <p className='skills'>React Native</p>
+                    <span></span>
+                    <p className='skills'>Git</p>
+                    <p className='skills'>Redux</p>
                 </div>
             </div>
         </div>
