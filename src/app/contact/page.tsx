4572e63043e8b0215e750d925e2c9b992850f0ae
Jenish <jpat534@gmail.com>
2024-05-24 14:43:44 +1000

    Added a Contact Me form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,16 @@ const Page = () => {
 
     return (
         <div className='flex flex-col min-h-screen items-center bg-stone-900 text-white'>
-            <h1>Contact</h1>
+            <h1 className='my-4 text-5xl p-3 w-full text-center'>Contact Me</h1>
+            <form className='flex flex-col'>
+                <label>Name</label>
+                <input placeholder='John Doe'></input>
+                <label>Email</label>
+                <input placeholder='email@gmail.com'></input>
+                <label>Message</label>
+                <textarea name='content' />
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
