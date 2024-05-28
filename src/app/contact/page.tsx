f8be8b6e9fae7dbc83bef95b4a61de616d1e0ec9
Jenish <jpat534@gmail.com>
2024-05-28 12:37:51 +1000

    Adjusted the positioning of the contact form on the About page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Page = () => {
 
     return (
-        <div className='flex flex-col min-h-screen items-center bg-stone-900 text-white'>
-            <h1 className='my-4 text-5xl p-3 w-full text-center'>Contact Me</h1>
-            <form className='flex flex-col w-2/6'>
+        <div className='flex flex-col min-h-screen items-center bg-stone-900 text-white '>
+            <h1 className='my-4 text-5xl p-3 w-full text-center mt-10'>Contact Me</h1>
+            <form className='flex flex-1 flex-col w-2/6 mt-20'>
                 <label>Name</label>
                 <input placeholder='John Doe'></input>
                 <label>Email</label>
