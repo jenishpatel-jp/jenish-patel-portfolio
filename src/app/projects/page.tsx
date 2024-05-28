0a1a5494325416a4aad15bbf82c09d07336e088c
Jenish <jpat534@gmail.com>
2024-05-28 10:36:12 +1000

    Updated the screenshot images on the Projects page as well as fixing the view for mobile users

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,40 +6,56 @@ const Page = () => {
     return (
         <div className='flex flex-col min-h-screen items-center bg-stone-900 text-white'>
             <h1 className='my-4 text-5xl p-3 w-full text-center'>Projects</h1>
-            <div className='grid grid-cols-2 grid-rows-3 gap-4 mb-4 mt-2 flex-1 p-2 w-5/6 text-center'>
-                <div className='flex items-center justify-center p-2'>
+            <div className='flex flex-col mb-4 mt-2 p-2 w-5/6 text-center'>
+                <div className='flex flex-col lg:flex-row mb-8'>
+                    <div className='flex items-center justify-center p-2  flex-1'>
+                        <Image
+                            src={"/Jammming screenshot.png"}
+                            alt="Screenshot of the Jammming app"
+                            width={900}
+                            height={500}
+                        />
+                    </div>
+                    <div className='flex flex-col p-2 items-center justify-center flex-1'>
+                        <h2 className='text-4xl p-2 font-medium' >Jammming</h2>
+                        <p className='p-2 text-lg mt-3' >A web app application that integrates with Spotify, allowing you to search for songs on the platform and create a new playlist on your Spotify account.</p>
+                        <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://jammming-spotify-project.vercel.app/'} target='_blank'>LIVE APP</Link>
+                        <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://github.com/jenishpatel-jp/jammming-spotify-project'} target='_blank'>LEARN MORE</Link>
+                    </div>
+                </div>
+                <div className='flex flex-col lg:flex-row mb-8'>
+                    <div className='flex items-center justify-center p-2 rounded-md flex-1'>
+                        <Image
+                            src={"/Reddit mini screenshot.png"}
+                            alt="Screenshot of the Reddit mini app"
+                            width={900}
+                            height={500}
+                        />
+                    </div>
+                    <div className='flex flex-col p-2 items-center justify-center flex-1'>
+                        <h2 className='text-4xl p-2 font-medium' >Reddit mini</h2>
+                        <p className='p-2 text-lg mt-3' >A web application that integrates with Reddit, fetching Reddit posts and has a sidebar allowing you to navigate from the selected subreddits.</p>
+                        <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://reddit-mini-five.vercel.app/'} target='_blank'>LIVE APP</Link>
+                        <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://github.com/jenishpatel-jp/reddit-mini'} target='_blank'>LEARN MORE</Link>
+                    </div>
+                </div>
+                <div className='flex flex-col lg:flex-row mb-8' >
+                    <div className='flex  justify-center items-center flex-1'>
                     <Image
-                        src={"/jammming-screenshot.png"}
-                        alt="Screenshot of the Jammming app"
-                        width={1000}
-                        height={500}
-                    />
-                </div>
-                <div className='flex flex-col p-2 items-center justify-center'>
-                    <p className='p-2 text-lg mt-3' >A web app application that integrates with Spotify, allowing you to search for songs on the platform and create a new playlist on your Spotify account.</p>
-                    <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://jammming-spotify-project.vercel.app/'} target='_blank'>LIVE APP</Link>
-                    <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://github.com/jenishpatel-jp/jammming-spotify-project'} target='_blank'>LEARN MORE</Link>
-                </div>
-                <div className=' justify-center p-2 rounded-md'>
-                    <Image
-                        src={"/reddit-mini-screenshot.png"}
-                        alt="Screenshot of the Jammming app"
-                        width={1000}
-                        height={500}
-                    />
-                </div>
-                <div className='flex flex-col p-2 items-center justify-center'>
-                    <p className='p-2 text-lg mt-3' >A web application that integrates with Reddit, fetching Reddit posts and has a sidebar allowing you to navigate from the selected subreddits.</p>
-                    <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://reddit-mini-five.vercel.app/'} target='_blank'>LIVE APP</Link>
-                    <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://github.com/jenishpatel-jp/reddit-mini'} target='_blank'>LEARN MORE</Link>
-                </div>
-                <div className='border'>
-                    <h2>Shopping Run</h2>
-                </div>
-                <div className='flex flex-col p-2 border items-center justify-center'>
-                    <p className='p-2 text-lg mt-3' >A mobile application created for your shopping run. The app allows you to separate items based on the stores you&apos;re going to.</p>
-                    <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={''} target='_blank'>LIVE APP</Link>
-                    <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://github.com/jenishpatel-jp/shopping-run'} target='_blank'>LEARN MORE</Link>
+                            src={"/Shopping run screenshot.png"}
+                            alt="Screenshot of the Shopping Run app"
+                            width={300}
+                            height={600}
+                            className=' justify-center'
+                        />
+                    </div>
+                    <div className='flex flex-col p-2  items-center justify-center flex-1'>
+                        <h2 className='text-4xl p-2 font-medium' >Shopping Run</h2>
+                        <p className='p-2 text-lg mt-3' >A mobile application created for your shopping run. The app allows you to separate items based on the stores you&apos;re going to.</p>
+                        <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={''} target='_blank'>LIVE APP</Link>
+                        <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://github.com/jenishpatel-jp/shopping-run'} target='_blank'>LEARN MORE</Link>
+                    </div>
+
                 </div>
             </div>
         </div>
