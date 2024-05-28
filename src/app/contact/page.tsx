a7c595220dd4b45730ecdadee9b55e4bb7ac7ddc
Jenish <jpat534@gmail.com>
2024-05-28 16:52:26 +1000

    Updated the styling on the contact page.
    Added padding, marings and underline colour.
    
    Also changed the position of the Submit button

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,13 +6,15 @@ const Page = () => {
         <div className='flex flex-col min-h-screen items-center bg-stone-900 text-white '>
             <h1 className='my-4 text-5xl p-3 w-full text-center mt-10 underline underline-offset-8 underline-orange'>Contact</h1>
             <form className='flex flex-1 flex-col w-2/6 mt-20'>
-                <label>Name</label>
-                <input placeholder='John Doe'></input>
-                <label>Email</label>
-                <input placeholder='email@gmail.com'></input>
-                <label>Message</label>
-                <textarea name='content' />
-                <button className='border rounded my-4 p-4 items-end w-fit'>Submit</button>
+                <label className='p-2 text-lg'>Name</label>
+                <input placeholder='John Doe' className='p-2'></input>
+                <label className='p-2 text-lg'>Email</label>
+                <input placeholder='email@gmail.com' className='p-2'></input>
+                <label className='p-2 text-lg'>Message</label>
+                <textarea name='content'  className='p-2'/>
+                <div className='flex flex-row-reverse my-2'>
+                    <button className='rounded my-4 p-4 items-end w-fit text-xl underline underline-offset-8 underline-orange hover:bg-orange-400'>Submit</button>
+                </div>
             </form>
         </div>
     )
