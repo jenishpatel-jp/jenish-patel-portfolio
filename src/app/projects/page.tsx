5d5855ad3f8e40c7636507f6a405d2cf5f92ae99
Jenish <jpat534@gmail.com>
2024-07-05 14:12:32 +1000

    Updated the Projects page so the Shopping Run project is at the top and Jamming project is at the bottom

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,20 +6,21 @@ const Page = () => {
         <div className='flex flex-col min-h-screen items-center bg-stone-900 text-white'>
             <h1 className='my-4 text-5xl p-3 w-full text-center underline underline-offset-8 underline-orange'>Projects</h1>
             <div className='flex flex-col mb-4 mt-2 p-2 w-5/6 text-center'>
-                <div className='flex flex-col lg:flex-row mb-8'>
-                    <div className='flex items-center justify-center p-2  flex-1'>
-                        <Image
-                            src={"/project-screenshots/Jammming screenshot.png"}
-                            alt="Screenshot of the Jammming app"
-                            width={900}
-                            height={500}
+                <div className='flex flex-col lg:flex-row mb-8' >
+                    <div className='flex  justify-center items-center flex-1'>
+                    <Image
+                            src={"/project-screenshots/Shopping run screenshot.png"}
+                            alt="Screenshot of the Shopping Run app"
+                            width={300}
+                            height={600}
+                            className=' justify-center'
                         />
                     </div>
-                    <div className='flex flex-col p-2 items-center justify-center flex-1'>
-                        <h2 className='text-4xl p-2 font-medium' >Jammming</h2>
-                        <p className='p-2 text-lg mt-3' >A Spotify integrated web application, allowing you to search for songs on the platform and create a new playlist on your Spotify account.</p>
-                        <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://jammming-spotify-project.vercel.app/'} target='_blank'>LIVE APP</Link>
-                        <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://github.com/jenishpatel-jp/jammming-spotify-project'} target='_blank'>LEARN MORE</Link>
+                    <div className='flex flex-col p-2  items-center justify-center flex-1'>
+                        <h2 className='text-4xl p-2 font-medium' >Shopping Run</h2>
+                        <p className='p-2 text-lg mt-3' >A mobile application created using Expo and React Native for your shopping run. The app allows you to separate items based on the stores you&apos;re going to.</p>
+                        {/* <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={''} target='_blank'>LIVE APP</Link> */}
+                        <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://github.com/jenishpatel-jp/shopping-run'} target='_blank'>LEARN MORE</Link>
                     </div>
                 </div>
                 <div className='flex flex-col lg:flex-row mb-8'>
@@ -38,21 +39,20 @@ const Page = () => {
                         <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://github.com/jenishpatel-jp/reddit-mini'} target='_blank'>LEARN MORE</Link>
                     </div>
                 </div>
-                <div className='flex flex-col lg:flex-row mb-8' >
-                    <div className='flex  justify-center items-center flex-1'>
-                    <Image
-                            src={"/project-screenshots/Shopping run screenshot.png"}
-                            alt="Screenshot of the Shopping Run app"
-                            width={300}
-                            height={600}
-                            className=' justify-center'
+                <div className='flex flex-col lg:flex-row mb-8'>
+                    <div className='flex items-center justify-center p-2  flex-1'>
+                        <Image
+                            src={"/project-screenshots/Jammming screenshot.png"}
+                            alt="Screenshot of the Jammming app"
+                            width={900}
+                            height={500}
                         />
                     </div>
-                    <div className='flex flex-col p-2  items-center justify-center flex-1'>
-                        <h2 className='text-4xl p-2 font-medium' >Shopping Run</h2>
-                        <p className='p-2 text-lg mt-3' >A mobile application created using Expo and React Native for your shopping run. The app allows you to separate items based on the stores you&apos;re going to.</p>
-                        {/* <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={''} target='_blank'>LIVE APP</Link> */}
-                        <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://github.com/jenishpatel-jp/shopping-run'} target='_blank'>LEARN MORE</Link>
+                    <div className='flex flex-col p-2 items-center justify-center flex-1'>
+                        <h2 className='text-4xl p-2 font-medium' >Jammming</h2>
+                        <p className='p-2 text-lg mt-3' >A Spotify integrated web application, allowing you to search for songs on the platform and create a new playlist on your Spotify account.</p>
+                        <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://jammming-spotify-project.vercel.app/'} target='_blank'>LIVE APP</Link>
+                        <Link className='p-2 m-2 text-xl underline underline-offset-8 underline-orange hover:bg-orange-400 rounded-md' href={'https://github.com/jenishpatel-jp/jammming-spotify-project'} target='_blank'>LEARN MORE</Link>
                     </div>
                 </div>
             </div>
