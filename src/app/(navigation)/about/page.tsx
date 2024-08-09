c0285cc3e1b996952050508b0580a1614bf846ea
Jenish <jpat534@gmail.com>
2024-08-09 16:14:34 +1000

    Updated the logos to include MongoDB and shrunk the logo size

--- src/app/(navigation)/about/page.tsx
+++ src/app/(navigation)/about/page.tsx
@@ -4,7 +4,7 @@ const Page = () => {
     return (
         <div className='flex flex-col min-h-screen items-center bg-zinc-900 text-white'>
             <h1 className='my-4 text-5xl p-3 w-full text-center underline underline-offset-8 underline-fuchsia' >About</h1>
-            <div className='flex flex-col lg:flex-row w-5/6 mx-3 mt-4 mb-7 flex-1'>
+            <div className='flex flex-col lg:flex-row w-5/6 mx-3 mt-4 mb-7 flex-1 items-center'>
                 <div className='flex lg:flex-col items-center lg:w-3/5' >
                     <p className='text-left text-xl lg:mt-10 lg:pt-10'>
                         Driven by a quest for continous self-improvement, I am on a journey to find a fulfilling career that aligns with my passions. 
@@ -29,8 +29,8 @@ const Page = () => {
                         <div className='skills' >
                             <Image 
                             src={'/skills-logo/HTML5-high.png'}
-                            width={150}
-                            height={150}
+                            width={100}
+                            height={100}
                             alt='HTML'
                             className='p-2 m-2'
                             />
@@ -39,8 +39,8 @@ const Page = () => {
                         <div className='skills' >
                         <Image 
                         src={'/skills-logo/css-high.png'}
-                        width={150}
-                        height={150}
+                        width={100}
+                        height={100}
                         alt='CSS'
                         className='p-2 m-2'
                         />
@@ -49,8 +49,8 @@ const Page = () => {
                         <div className='skills' >
                         <Image 
                         src={'/skills-logo/Javascript.png'}
-                        width={150}
-                        height={150}
+                        width={100}
+                        height={100}
                         alt='Javascript'
                         className='p-2 m-2'
                         />
@@ -59,8 +59,8 @@ const Page = () => {
                         <div className='skills' >
                             <Image 
                             src={'/skills-logo/Typescript_logo_2020.svg'}
-                            width={150}
-                            height={150}
+                            width={100}
+                            height={100}
                             alt='Redux'
                             className='p-2 m-2'
                             />
@@ -72,8 +72,8 @@ const Page = () => {
                         <div className='skills' >
                             <Image 
                             src={'/skills-logo/React.png'}
-                            width={150}
-                            height={150}
+                            width={100}
+                            height={100}
                             alt='React'
                             className='p-2 m-2'
                             />
@@ -82,8 +82,8 @@ const Page = () => {
                         <div className='skills' >
                             <Image 
                             src={'/skills-logo/nextjs-logo.png'}
-                            width={150}
-                            height={150}
+                            width={100}
+                            height={100}
                             alt='Next.JS'
                             className='p-2 m-2'
                             />
@@ -92,8 +92,8 @@ const Page = () => {
                         <div className='skills' >
                             <Image 
                             src={'/skills-logo/tailwind-css.png'}
-                            width={150}
-                            height={150}
+                            width={100}
+                            height={100}
                             alt='Redux'
                             className='p-2 m-2'
                             />
@@ -102,8 +102,8 @@ const Page = () => {
                         <div className='skills' >
                             <Image 
                             src={'/skills-logo/git.png'}
-                            width={150}
-                            height={150}
+                            width={100}
+                            height={100}
                             alt='Git'
                             className='p-2 m-2'
                             />
@@ -114,8 +114,8 @@ const Page = () => {
                         <div className='skills' >
                             <Image 
                             src={'/skills-logo/redux.png'}
-                            width={150}
-                            height={150}
+                            width={100}
+                            height={100}
                             alt='Redux'
                             className='p-2 m-2'
                             />
@@ -124,8 +124,8 @@ const Page = () => {
                         <div className='skills' >
                             <Image 
                             src={'/skills-logo/react-native.png'}
-                            width={150}
-                            height={150}
+                            width={100}
+                            height={100}
                             alt='React Native'
                             className='p-2 m-2'
                             />
@@ -134,8 +134,8 @@ const Page = () => {
                         <div className='skills' >
                             <Image 
                             src={'/skills-logo/expo-go-app.svg'}
-                            width={150}
-                            height={150}
+                            width={100}
+                            height={100}
                             alt='Expo Go'
                             className='p-2 m-2 flex-1'
                             />
@@ -144,8 +144,8 @@ const Page = () => {
                         <div className='skills' >
                             <Image 
                             src={'/skills-logo/nodejs_light.svg'}
-                            width={150}
-                            height={150}
+                            width={100}
+                            height={100}
                             alt='Expo Go'
                             className='p-2 m-2 flex-1'
                             />
@@ -156,12 +156,22 @@ const Page = () => {
                         <div className='skills' >
                             <Image 
                             src={'/skills-logo/postgres.png'}
-                            width={150}
-                            height={150}
+                            width={100}
+                            height={100}
                             alt='Expo Go'
                             className='p-2 m-2 flex-1'
                             />
                             <p className='hidden lg:inline'>PostgreSQL</p>
+                        </div>
+                        <div className='skills' >
+                            <Image 
+                            src={'/skills-logo/mongodb.svg'}
+                            width={100}
+                            height={100}
+                            alt='MongoDB'
+                            className='p-2 m-2 flex-1'
+                            />
+                            <p className='hidden lg:inline'>MongoDB</p>
                         </div>
                     </div>  
                 </div>
