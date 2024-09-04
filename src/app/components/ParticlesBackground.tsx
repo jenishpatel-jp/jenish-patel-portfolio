1b8b7a68acdccdbdec2ac3d603c8583885c014ce
Jenish <jpat534@gmail.com>
2024-09-04 15:21:21 +1000

    installed tsparticles

---
+++ src/app/components/ParticlesBackground.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Particles from '@tsparticles/react';
+import { Engine } from '@tsparticles/engine';
+
+const ParticlesBackground: React.FC = () => {
+  return (
+    <Particles 
+
+    
+    
+    />
+  )
+}
+
+export default ParticlesBackground
