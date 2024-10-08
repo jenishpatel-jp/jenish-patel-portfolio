3d955c4470b54e0d7ce96a31ac7ae1e922095526
Jenish <jpat534@gmail.com>
2024-10-08 14:54:21 +1100

    Update the profolio so it no longer says im a full stack developer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center justify-center bg-stark">
         <h2 className="text-void w-full text-center text-5xl mb-5 " >
-          Hello, I&apos;m <span className="text-blue-500" >Jenish</span>.
-        </h2>
-        <h2 className="text-void w-full text-center text-5xl">
-          I&apos;m a Full Stack Developer.
+          Hello, I&apos;m <span className="text-blue-500" >Jenish Patel</span>.
         </h2>
         <Link href={'/projects'}>
           <button className="btn bg-blue-500 text-stark mt-6 rounded-xl shadow-lg text-lg drop-shadow-lg">View Projects</button>
