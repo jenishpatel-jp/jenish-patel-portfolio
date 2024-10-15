8e13687dca0b391b048adae572bc6da4b7374edc
Jenish <jpat534@gmail.com>
2024-10-15 12:09:47 +1100

    Update the Home page so it says I'm an aspiring junior developer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
         <h2 className="text-void w-full text-center text-5xl mb-5 " >
           Hello, I&apos;m <span className="text-blue-500" >Jenish Patel</span>.
         </h2>
+        <h2 className="text-void w-full text-center text-5xl mb-5 ">
+          I&apos;m an aspiring Junior Developer.
+        </h2>
         <Link href={'/projects'}>
           <button className="btn bg-blue-500 text-stark mt-6 rounded-xl shadow-lg text-lg drop-shadow-lg">View Projects</button>
         </Link>
