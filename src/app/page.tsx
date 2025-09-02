ec1307455053dcc10c64f7e808bcb7e0faa4a155
Jenish <jpat534@gmail.com>
2025-09-02 18:48:12 +1000

    Added dividers to show different pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center justify-center bg-stark">
-        <h2 className="text-void w-full text-center text-5xl mb-5 " >
-          Hello, I&apos;m <span className="text-blue-500" >Jenish Patel</span>.
-        </h2>
-        <h2 className="text-void w-full text-center text-5xl mb-5 ">
-          I&apos;m an aspiring Junior Developer.
-        </h2>
-        <Link href={'/projects'}>
-          <button className="btn bg-blue-500 text-stark mt-6 rounded-xl shadow-lg text-lg drop-shadow-lg">View Projects</button>
-        </Link>
-        
+      <div className="w-full h-screen bg-blue-200" ></div>
+      <div className="w-full h-screen bg-green-200" ></div>
+      <div className="w-full h-screen bg-orange-200" ></div>
+      <div className="w-full h-screen bg-yellow-200" ></div>
+
     </main>
   );
 }
