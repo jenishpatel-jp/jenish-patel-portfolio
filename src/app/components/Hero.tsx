2ac9c96fefab3531d1c5757c708e831c6bf5c75b
Jenish <jpat534@gmail.com>
2025-09-02 20:42:59 +1000

    Added a temporary landing page

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='text-xl items-center justify-center flex-1 flex h-full' >
+        <h1 className='text-black'>Jenish Patel's landing </h1>
+    </div>
+  )
+}
+
+export default Hero
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center justify-center bg-stark">
-      <div className="w-full h-screen bg-blue-200" ></div>
+      <div className="w-full h-screen bg-blue-200" >
+        <Hero />
+      </div>
       <div className="w-full h-screen bg-green-200" ></div>
       <div className="w-full h-screen bg-orange-200" ></div>
       <div className="w-full h-screen bg-yellow-200" ></div>
