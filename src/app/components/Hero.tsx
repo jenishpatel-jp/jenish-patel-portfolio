332c4941dad35fa274f229ee3cb5de3e8a98bc3f
Jenish <jpat534@gmail.com>
2025-09-02 20:48:19 +1000

    Updating the ' to &apos; so it doesn't throw an error

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='text-xl items-center justify-center flex-1 flex h-full' >
-        <h1 className='text-black'>Jenish Patel's landing </h1>
+        <h1 className='text-black'>Jenish Patel&apos;s landing </h1>
     </div>
   )
 }
