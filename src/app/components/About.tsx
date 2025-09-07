4945de12f04e0658509b258957cb2b281b7a6620
Jenish <jpat534@gmail.com>
2025-09-07 14:10:23 +1000

    Added an About page component

---
+++ src/app/components/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='text-xl items-center justify-center flex-1 flex h-full' >
+        <h1 className='text-black'>About Page</h1>
+    </div>
+  )
+}
+
+export default About
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,16 @@ import Hero from "./components/Hero";
 import ShoppingRun from "./components/ShoppingRun";
 import STT from "./components/STT";
 import Contact from "./components/Contact";
+import About from "./components/About";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center justify-center bg-stark">
       <div className="w-full h-screen bg-blue-200" >
         <Hero />
+      </div>
+      <div className="w-full h-screen bg-purple-200" >
+        <About />
       </div>
       <div className="w-full h-screen bg-green-200" >
         <ShoppingRun />
