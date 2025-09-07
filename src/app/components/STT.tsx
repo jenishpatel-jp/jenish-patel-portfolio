fcd50082e76af5b71717f86deae813f792c57418
Jenish <jpat534@gmail.com>
2025-09-07 15:35:30 +1000

    uploaded a next image of the shopping run screenshot

--- src/app/components/STT.tsx
+++ src/app/components/STT.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image';
 import React from 'react'
 
 const STT = () => {
   return (
     <div className='text-xl items-center justify-center flex-1 flex h-full' >
-        <h1 className='text-black'>Shroomless Time Trial </h1>
+        <Image src="/images/stt/STT-4.png" alt="Shopping Run Screenshot" width={400} height={600} />
     </div>
   )
 }
