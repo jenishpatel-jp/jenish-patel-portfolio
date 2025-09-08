ba8581f2982865186ed95fd36fee0e0b429a7380
Jenish <jpat534@gmail.com>
2025-09-08 18:27:06 +1000

    installed the Carousel package and added the Carousel to the shopping lisst component

--- src/app/components/ShoppingRun.tsx
+++ src/app/components/ShoppingRun.tsx
@@ -1,10 +1,33 @@
 import Image from 'next/image';
 import React from 'react';
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
 const ShoppingRun = () => {
   return (
     <div className='text-xl items-center justify-center flex-1 flex h-full' >
-        <Image src="/images/shopping-run/shopping-run-1.png" alt="Shopping Run Screenshot" width={400} height={600} />
+
+        <Carousel>
+          <CarouselContent>
+            <CarouselItem className='flex justify-center'>
+              <Image src="/images/shopping-run/shopping-run-1.png" alt="Shopping Run Screenshot" width={400} height={600} />
+            </CarouselItem>
+            <CarouselItem className='flex justify-center'>
+              <Image src="/images/shopping-run/shopping-run-2.png" alt="Shopping Run Screenshot" width={400} height={600} />
+            </CarouselItem>
+            <CarouselItem className='flex justify-center'>
+              <Image src="/images/shopping-run/shopping-run-3.png" alt="Shopping Run Screenshot" width={400} height={600} />
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
     </div>
   )
 }
