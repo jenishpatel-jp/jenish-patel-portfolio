d418be1cf6e798da1acf2bcd0d4c84354afadade
Jenish <jpat534@gmail.com>
2025-09-14 19:06:55 +1000

    Updated the hero page with text about who I am and my skills

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,8 +4,9 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='text-xl items-center justify-center flex-1 flex h-full' >
-        <h1 className='text-black'>Jenish Patel&apos;s landing </h1>
+    <div className='text-xl items-center justify-center flex-1 flex h-full flex-col' >
+        <h1 className='text-black text-5xl p-5'>Hello, I&apos;m Jenish Patel</h1>
+        <h1 className='text-black text-5xl p-5'>I am a full stack web and mobile developer</h1>
     </div>
   )
 }
