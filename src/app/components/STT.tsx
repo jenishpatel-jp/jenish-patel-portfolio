f94b7694d7aacfd912a0fa8c001e22b5351843c5
Jenish <jpat534@gmail.com>
2025-09-16 20:30:47 +1000

    Added buttons to the STT app page

--- src/app/components/STT.tsx
+++ src/app/components/STT.tsx
@@ -8,6 +8,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import { Button } from '@/components/ui/button';
 
 const STT = () => {
   return (
@@ -42,7 +43,13 @@ const STT = () => {
         </Carousel>
         <p className='text-lg'>This is my Shroomless Time Trial app. Designed to help keep track of your mario kart 8 shroomless time trial times</p>
         <p className='text-lg'>Technologies: Expo, React Native Framework</p>
-
+        <div className='flex space-x-10 mt-5 justify-center items-center'>
+          <Button className='w-25 h-10 rounded-lg justify-center items-center flex'>
+            <Image src="/images/github/github-mark-white.png" alt="GitHub logo" width={20} height={20} className='mr-2'/>
+              GitHub
+          </Button>
+          <Button className='w-25 h-10 rounded-lg justify-center items-center'>Android SDK</Button>
+        </div>
     </div>
   )
 }
